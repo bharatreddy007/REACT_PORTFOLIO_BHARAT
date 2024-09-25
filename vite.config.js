d9fcb80a61a04aa448f24b6087b3b0d92234a4aa
bharatreddy007 <saibharatreddy007@gmail.com>
2024-09-24 22:54:56 -0400

    2ndcommit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+  base: "/REACT_PORTFOLIO_BHARAT/", // YOUR REPO NAME HERE
 });
